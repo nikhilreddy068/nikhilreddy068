f1d77b1672f322d2a4281412126fb34da52df7f3
nikhilreddy068 <163346509+nikhilreddy068@users.noreply.github.com>
2024-03-14 01:12:17 +0530

    Add files via upload

---
+++ distance.py
@@ -0,0 +1,8 @@
+#distance between two points
+
+x1=eval(input("enter value of x1: "))
+x2=eval(input("enter value of x2: "))
+y1=eval(input("enter velue of y2: "))
+y2=eval(input("enter value of y2: "))
+dist=(((x2-x1)**2+(y2-y1)**2)**0.5)
+print(dist)
